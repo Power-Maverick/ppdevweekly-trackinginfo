32222785bf401833f45b550a4702d81fd2dddfb2
Power-Maverick <danish.naglekar@hotmail.com>
2021-02-19 17:28:38 -0500

    Changes to footer & header + css

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -6,7 +6,7 @@ export default class Footer extends Component {
       <footer>
         <p>
             <span>
-                Developed and maintained with <i className="fa fa-heart" /> by <a target="_blank" href="https://powermaverick.dev">Danish Naglekar</a> <a target="_blank" href="https://www.twitter.com/DanzMaverick"><i className="fa fa-twitter"></i></a> <a target="_blank" className="text-dark" href="https://www.linkedin.com/in/danishnaglekar/"><i className="fa fa-linkedin"></i></a>
+                Developed and maintained with <i className="fa fa-heart" /> by <a target="_blank" href="https://powermaverick.dev">Danish Naglekar</a> <a target="_blank" href="https://www.twitter.com/DanzMaverick"><i className="fa fa-twitter white"></i></a> <a target="_blank" className="text-dark" href="https://www.linkedin.com/in/danishnaglekar/"><i className="fa fa-linkedin white"></i></a>
             </span>
         </p>
       </footer>
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -11,7 +11,7 @@ export default class Header extends Component {
             <ul className="menu-bar">
                 <li><a href="#Blogs">Blogs</a></li>
                 <li><a href="#YouTube">Videos</a></li>
-                <li><a href="https://www.powerplatformdevelopersweekly.com/">Subscribe to newsletter</a></li>
+                <li><a href="https://www.powerplatformdevelopersweekly.com/" target="_blank">Subscribe to newsletter</a></li>
                 <li><a href="https://forms.office.com/Pages/ResponsePage.aspx?id=IwrCMdIujUa6q0Lt-ZgSi1zAstB7LDJArjOy-AmXSbxUNjJBRkJGUDZLVFFGMEVQNVFJTks5QTcyQy4u" target="_blank">Submit your content</a></li>
                 <li><a href="https://github.com/Power-Maverick/ppdevweekly-trackinginfo/issues/new?labels=data-issue&template=data-issue-report.md" target="_blank">Report issue</a></li>
             </ul>
